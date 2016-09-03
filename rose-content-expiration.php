fe37fb7f0014b5c9031fb01bafdf292990845764
Megan Morsie <megmorsie@users.noreply.github.com>
2016-09-03 16:56:24 -0400

    Update rose-content-expiration.php
    Fixed indentation.

--- rose-content-expiration.php
+++ rose-content-expiration.php
@@ -12,23 +12,23 @@
 if ( !(function_exists(rose_content_expiration_shortcode)) ) {
 	function rose_content_expiration_shortcode( $atts, $content ) {
 		$a = shortcode_atts( array(
-	        'start' => '1 January 1970 00:00:00', // 0000000000, lowest Unix time
-	        'end' => '19 January 2038 03:14:07', // 2147483647, highest Unix time
-	    ), $atts );
+			'start' => '1 January 1970 00:00:00', // 0000000000, lowest Unix time
+			'end' => '19 January 2038 03:14:07', // 2147483647, highest Unix time
+		), $atts );
 
-	    $start_time = strtotime( $a['start'] );
-	    $end_time = strtotime( $a['end'] );
+		$start_time = strtotime( $a['start'] );
+		$end_time = strtotime( $a['end'] );
 
-	    // Set Timezone
-	    $timezone = get_option('timezone_string');
-	    date_default_timezone_set($timezone); 
-	    $today = time();
+		// Set Timezone
+		$timezone = get_option('timezone_string');
+		date_default_timezone_set($timezone); 
+		$today = time();
 
-	    // Support Use of Shortcodes Within [temp]
-	    $content = do_shortcode($content);
+		// Support Use of Shortcodes Within [temp]
+		$content = do_shortcode($content);
 
-	    if ( ($today < $start_time) || ($today > $end_time) ) return;
-	    return $content;
+		if ( ($today < $start_time) || ($today > $end_time) ) return;
+		return $content;
 	}
 	add_shortcode( 'temp', 'rose_content_expiration_shortcode' );
 }
